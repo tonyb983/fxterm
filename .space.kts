5895a32fc24172fc6bc1c9b646642c10a3452d49
Tony Barbitta <tonyb983@gmail.com>
2021-02-01 04:26:57 +0000

    changing space

--- .space.kts
+++ .space.kts
@@ -4,4 +4,8 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-
+job("Run Qodana") {
+ 	container("jetbrains/qodana:2020.3-eap") {
+        args("-v ${}:/data/project -v ${GITHUB_WORKSPACE}/qodana:/data/results --save-report")
+    }
+}
